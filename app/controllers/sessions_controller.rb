b075f0558a33f1d8e7ba60f2ddc2eae1ffcb9313
Brittany Blake <brittany.blake49@gmail.com>
2020-05-29 12:22:49 +0200

    Create sessions & login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,15 @@ class SessionsController < ApplicationController
     end
 
     def create
-        byebug
+        user = User.find_by(email:params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "Loggen in successfully"
+            redirect_to user
+        else
+            flash.now[:alert] = "There was something wrong with your account details"
+            render 'new'
+        end
     end
 
     def destroy
