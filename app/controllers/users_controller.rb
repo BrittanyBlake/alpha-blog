8b9debff33e76782df97ebe38156737e12f027af
Brittany Blake <brittany.blake49@gmail.com>
2020-05-31 11:41:52 +0200

    Add Authentication helper methods and dry code in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController 
+  before_action :set_user, only:[:show, :edit, :update]
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
    def create
      @user = User.new(user_params)
      if @user.save
+       session[:user_id] = @user.id
        flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
        redirect_to articles_path
      else
@@ -24,11 +25,9 @@ class UsersController < ApplicationController
    end
 
    def edit
-    @user = User.find(params[:id])
    end
 
    def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
         flash[:notice] = "Your account information was successfully updated!"
         redirect_to @user
@@ -40,5 +39,9 @@ class UsersController < ApplicationController
    private
    def user_params
      params.require(:user).permit(:username, :email, :password)
+   end
+
+   def set_user
+    @user = User.find(params[:id])
    end
 end
