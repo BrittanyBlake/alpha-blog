6583193fe8e7cc0aa8f93cb111ab59b15147dd54
Brittany Blake <brittany.blake49@gmail.com>
2020-05-31 18:15:32 +0200

    restrict ability to modify articles from url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,11 @@ class ApplicationController < ActionController::Base
     def logged_in?
         !!current_user
     end
+
+    def require_user
+        if !logged_in?
+            flash[:alert] = "You must be logged in to perform this action"
+            redirect_to login_path
+        end
+    end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only:[:show, :edit, :update, :destroy]
+    before_action :require_user, except:[:show, :index]
+    before_action :require_same_user, only:[:edit, :update, :destroy]
 
     def show
     end
@@ -38,6 +40,13 @@ class ArticlesController < ApplicationController
     def destroy
         @article.destroy
         redirect_to articles_path
+    end
+
+    def require_same_user
+        if current_user != @article.user
+            flash[:alert] = "You must be logged in  as the ownder to perform this action"
+            redirect_to @article
+        end
     end
 
     private
