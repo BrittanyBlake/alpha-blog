d65df98c7cb79bcbbe1502c8c2df454dab7879ee
Brittany Blake <brittany.blake49@gmail.com>
2020-06-01 15:57:29 +0200

    Allow admin to delete and edit profiles/articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -53,8 +53,8 @@ class ArticlesController < ApplicationController
     end
 
      def require_same_user
-        if current_user != @article.user
-            flash[:alert] = "You can only edit your own articles"
+        if current_user != @article.user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete your own articles"
             redirect_to @article
         end
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
 
    def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Your account and all your artciles have been successfully deleted"
     redirect_to root_path
    end
@@ -58,8 +58,8 @@ class UsersController < ApplicationController
    end
 
    def require_same_user
-     if current_user != @user
-       flash[:alert] = "You can only edit your own account"
+     if current_user != @user && !current_user.admin?
+       flash[:alert] = "You can only edit or delete your own account"
        redirect_to @user
      end
    end
