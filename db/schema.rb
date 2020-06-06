bf36b0eee310b3244efc3c8dd2f3c40ceed86fb9
Brittany Blake <brittany.blake49@gmail.com>
2020-06-06 18:36:26 +0200

    Add category model and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_131747) do
+ActiveRecord::Schema.define(version: 2020_06_06_163346) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2020_06_01_131747) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
---
+++ test/models/category_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+    test "category should be valid" do
+        @category = Category.new(name:"sports")
+        assert @category.valid?
+    end
+end
