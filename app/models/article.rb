e3fdbec7bad9253ae245ac90bca0da516b2087a6
Brittany Blake <brittany.blake49@gmail.com>
2020-06-08 15:45:03 +0200

    remove duplicate article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    belongs_to :user
     validates :title, presence: true, length: {minimum: 6, maxmimum: 100}
     validates :description, presence: true, length: {minimum: 10, maxmimum: 300}
 end
