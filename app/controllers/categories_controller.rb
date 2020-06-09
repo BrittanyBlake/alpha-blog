1c47f89a53d25d928013d3dca1ace4e2f093e5ec
Brittany Blake <brittany.blake49@gmail.com>
2020-06-09 18:15:00 +0200

    edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,6 +23,20 @@ class CategoriesController < ApplicationController
             render 'new'
         end
     end
+    
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:notice] = "Category was successfully updates"
+            redirect_to @category
+        else
+            render 'edit'
+        end
+    end
 
     private
     def category_params
