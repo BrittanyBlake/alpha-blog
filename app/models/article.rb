b7b5cabb288655f192cd42c0cc406c273dad9698
Brittany Blake <brittany.blake49@gmail.com>
2020-06-09 13:07:59 +0200

    add many-to-many association backend for articles and categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
     belongs_to :user
     validates :title, presence: true, length: {minimum: 6, maxmimum: 100}
     validates :description, presence: true, length: {minimum: 10, maxmimum: 300}
+    has_many :article_categories
+    has_many :categories, through: :article_categories
 end
