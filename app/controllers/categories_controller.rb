c4eacae4a3da978e5bca9c2689faef9e78180b9e
Brittany Blake <brittany.blake49@gmail.com>
2020-06-09 17:49:11 +0200

    add category index and show views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,7 @@ class CategoriesController < ApplicationController
 
     def show
         @category = Category.find(params[:id])
+        @articles =  @category.articles.paginate(page: params[:page], per_page: 5)
     end
 
     def new
